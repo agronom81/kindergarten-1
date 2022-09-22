0d9c008785c15d9411b870c645ff8d4052488f4b
Pavlo Petrenko <agronom81@gmail.com>
2022-09-22 08:17:14 +0300

    add tailwind, main photo

---
+++ kindergarten/src/components/Dot.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="absolute w-4 h-4 rounded-full">
+    <button
+      class="absolute w-full h-full top-0 left-0 bg-white cursor-pointer"
+      type="button"
+    ></button>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  top: string;
+  left: string;
+}>();
+</script>
+
+<style scoped></style>
