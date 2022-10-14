13eb2c5b0a524885ba2c3bc54d61340404410796
Pavlo Petrenko <agronom81@gmail.com>
2022-10-14 08:02:32 +0300

    add new Content

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <main class="w-[958px] mx-auto relative h-screen">
-    <div class="flex justify-center items-center relative fade-in h-screen">
+  <main class="w-[958px] mx-auto relative">
+    <div class="flex justify-center items-center relative fade-in">
       <img class="block m-0" src="./assets/img/photo1.jpg" alt="image" />
-      <DotItem top="450px" left="579px" :content="dotContent.item1"></DotItem>
-      <DotItem top="587px" left="766px" :content="dotContent.item2"></DotItem>
-      <DotItem top="618px" left="480px" :content="dotContent.item3"></DotItem>
-      <DotItem top="424px" left="144px" :content="dotContent.item4"></DotItem>
+      <DotItem top="450px" left="577px" :content="dotContent.item1"></DotItem>
+      <DotItem top="587px" left="764px" :content="dotContent.item2"></DotItem>
+      <DotItem top="618px" left="478px" :content="dotContent.item3"></DotItem>
+      <DotItem top="424px" left="141px" :content="dotContent.item4"></DotItem>
     </div>
   </main>
 </template>
@@ -17,25 +17,29 @@ import calendar from "./assets/audio/calendar.mp3";
 import flowers from "./assets/audio/flowers.mp3";
 import fluger from "./assets/audio/fluger.mp3";
 import wind from "./assets/audio/wind.mp3";
+import windJpeg from "./assets/img/wind.jpeg";
+import flowersJpeg from "./assets/img/board.png";
+import calendarJpeg from "./assets/img/board2.png";
+import flugerJpeg from "./assets/img/fluger.jpeg";
 const dotContent = ref({
   item1: {
     label: "Вітряний рукав",
-    imgPath: "/src/assets/img/wind.jpeg",
+    imgPath: windJpeg,
     sound: wind,
   },
   item2: {
     label: "Квітник",
-    imgPath: "/src/assets/img/board.png",
+    imgPath: flowersJpeg,
     sound: flowers,
   },
   item3: {
     label: "Погода",
-    imgPath: "/src/assets/img/board2.png",
+    imgPath: calendarJpeg,
     sound: calendar,
   },
   item4: {
     label: "Флюгер",
-    imgPath: "/src/assets/img/fluger.jpeg",
+    imgPath: flugerJpeg,
     sound: fluger,
   },
 });
--- src/components/DotItem.vue
+++ src/components/DotItem.vue
@@ -1,13 +1,29 @@
 <template>
   <div
     ref="root"
-    class="absolute w-8 h-8 rounded-full dotItem border"
+    class="absolute w-10 h-10 rounded-full dotItem border"
     :style="{ top: top, left: left }"
   >
     <div
       v-show="isShow"
       class="flex justify-center items-center h-52 bg-white shadow-2xl p-4 top-[-12rem] left-[-87px] rounded-md fade-in w-52"
     >
+      <button class="close-btn" @click="closePopup">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-8 h-8"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </button>
       <img
         class="block w-full rounded-md"
         :src="content.imgPath"
@@ -52,15 +68,20 @@ const playSound = (sound: string) => {
   }
 };
 
+const closePopup = () => {
+  if (audio.value) {
+    audio.value?.pause();
+  }
+  isShow.value = !isShow.value;
+};
+
 const clickOutside = () => {
   const onClickOutside = (e: any) => {
     if (root.value) {
       if (!root.value?.contains(e.target)) {
         audio.value?.pause();
+        isShow.value = false;
       }
-
-      // audio.value?.currentTime = 0;
-      isShow.value = root.value?.contains(e.target);
     }
   };
   document.addEventListener("click", onClickOutside);
